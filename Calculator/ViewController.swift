822d525338dd5258fa964bfa238f7233109e3061
sriharsha sammeta <sriharsha4444@icloud.com>
2016-11-14 13:42:42 +0530

    Final Versio of calculator app

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+
+    var UserIsInTheMiddleOfTypingANumber = false
+    var brain = CalculatorBrain()
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        
+        let digit = sender.currentTitle!
+        if UserIsInTheMiddleOfTypingANumber {
+            if(display.text!.rangeOfString(".")?.startIndex == nil || digit != "."){
+                     display.text = display.text! + digit
+            }
+        }else{
+            display.text = digit
+            UserIsInTheMiddleOfTypingANumber = true
+            
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func operate(sender: UIButton) {
+        
+        if UserIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        if let operation = sender.currentTitle{
+            if let result = brain.performOperation(operation){
+                displayValue = result
+            }else{
+                displayValue = 0
+            }
+        }
     }
-
-
+    
+    @IBAction func enter() {
+        UserIsInTheMiddleOfTypingANumber = false
+        if let result = brain.pushOperand(displayValue){
+            displayValue = result
+        }else{
+            displayValue = 0
+        }
+        
+    }
+    
+    var displayValue: Double {
+        get{
+        return  NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set{
+        display.text = "\(newValue)"
+        UserIsInTheMiddleOfTypingANumber = false
+        }
+    }
+    
 }
 
